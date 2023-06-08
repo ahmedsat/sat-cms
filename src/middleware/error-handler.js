1bb9edd511eb8f14b8571fe6f44d9aebab8887c7
Ahmed Sat <ahmedsat@debian.sat>
2023-06-08 21:28:22 +0300

    duplicate error and cast error

--- src/middleware/error-handler.js
+++ src/middleware/error-handler.js
@@ -5,7 +5,7 @@ export const ErrorHandler = (err, req, res, next) => {
     message: err.message || "Internal server error",
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
   };
-  //
+
   if (err.name === "ValidationError") {
     Error = {
       message: Object.values(err.errors)
@@ -14,7 +14,19 @@ export const ErrorHandler = (err, req, res, next) => {
       statusCode: StatusCodes.BAD_REQUEST,
     };
   }
-  //
+
+  if (err.code && err.code === 11000) {
+    Error.message = `Duplicate value entered for ${Object.keys(
+      err.keyValue
+    )} field, please choose another value`;
+    Error.statusCode = 400;
+  }
+
+  if (err.name === "CastError") {
+    Error.message = `No item found with id : ${err.value}`;
+    Error.statusCode = 404;
+  }
+
   return res.status(Error.statusCode).json({
     message: Error.message,
   });
