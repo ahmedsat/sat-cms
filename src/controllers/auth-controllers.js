ba10a8b02ce21d872c13bf087595ab15f53448a8
Ahmed Sat <ahmedsat@debian.sat>
2023-06-16 10:23:01 +0300

    make login.test.js pass - last one not passed yet

--- src/controllers/auth-controllers.js
+++ src/controllers/auth-controllers.js
@@ -1,6 +1,12 @@
 import { StatusCodes } from "http-status-codes";
 import { User } from "../models/User.js";
 import { CreateJWT } from "../utils/jwt.js";
+import {
+  notFoundError,
+  badRequestError,
+  unauthenticatedError,
+} from "../utils/errors.js";
+import { compareHashed } from "../utils/password.js";
 
 export const register = async (req, res) => {
   const user = await User.create({ ...req.body });
@@ -11,4 +17,35 @@ export const register = async (req, res) => {
     name: user.name,
     token: token,
   });
+};
+
+export const login = async (req, res) => {
+  const { username, email, id, password } = req.body;
+
+  // if now email or username or id provided
+  if ((!username && !email && !id) || !password)
+    throw badRequestError("Email and password are required");
+
+  let user;
+
+  if (id) {
+    user = await User.findById(id);
+  }
+
+  if (!user && username) {
+    user = await User.findOne({ username });
+  }
+
+  if (!user && email) {
+    user = await User.findOne({ email });
+  }
+
+  if (!user) throw notFoundError("User not found");
+
+  if (!(await compareHashed(password, user.password)))
+    throw unauthenticatedError("Invalid password");
+
+  const token = CreateJWT(user);
+
+  res.json({ name: user.name, id: user._id, token: token });
 };
--- src/routes/auth-routes.js
+++ src/routes/auth-routes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { register } from "../controllers/auth-controllers.js";
+import { register, login } from "../controllers/auth-controllers.js";
 
 export const authRouter = Router();
 
-// authRouter.post("/login", login);
+authRouter.post("/login", login);
 authRouter.post("/register", register);
 
 // authRouter.get("/me", auth, me);
